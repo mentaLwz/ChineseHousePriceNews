0d93a27470d42a11afff6cbfbec3a7dc0b4087af
mentalwz <mentalwz@outlook.com>
2024-02-21 23:31:06 +0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header class="shadow-sm bg-white">
+      <nav class="container text-lg mx-auto p-4 flex justify-between">
+        <NuxtLink to="/">中国房价信息汇总</NuxtLink>
+        <ul class="flex gap-4">
+          <li><NuxtLink to="/about">关于</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="container mx-auto p-4">
+      <slot ></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p class="mx-auto font-extralight">只关注中国房价相关信息</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
